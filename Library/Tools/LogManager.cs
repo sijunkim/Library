2b57ffe0cdff297f4d896a394ffeeb2e615a3d53
김시준 <papaya9349@naver.com>
2016-09-14 15:26:32 +0900

    Add project files.

---
+++ Library/Tools/LogManager.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Library.Tools
+{
+    public class LogManager
+    {
+        private string path;
+
+        #region Constructors
+        public LogManager(string path)
+        {
+            this.path = path;
+            SetLogPath();
+        }
+
+        public LogManager()
+            : this(Path.Combine(Application.Root, "Log"))
+        {
+        }
+        #endregion
+
+        #region Methods
+        private void SetLogPath()
+        {
+            string logFile = string.Empty;
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            logFile = DateTime.Now.ToString("yyyyMMdd") + ".txt";
+            path = Path.Combine(path, logFile);
+        }
+
+        public void Write(string data)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path, true))
+                {
+                    writer.Write(data);
+                }
+            }
+            catch { }
+        }
+
+        public void WriteLine(string data)
+        {
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(path, true))
+                {
+                    writer.WriteLine(DateTime.Now.ToString("yyyyMMdd HH:mm:ss\t") + data);
+                }
+            }
+            catch { }
+        }
+        #endregion
+    }
+}
---
+++ LibraryTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Library.Tools;
+
+namespace LibraryTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LogManager log = new LogManager();
+
+            log.WriteLine("[Begin Processing]");
+
+            for (int index = 0; index < 10; index++)
+            {
+                log.WriteLine("Processing: " + index);
+
+                System.Threading.Thread.Sleep(500);
+
+                log.WriteLine("Done: " + index);
+            }
+
+            log.WriteLine("[End Processing]");
+        }
+    }
+}
